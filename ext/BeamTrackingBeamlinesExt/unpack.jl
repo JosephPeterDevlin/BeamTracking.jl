77b4302990f976b8132737570379905e0e98f8ef
Joseph Devlin <jpd285@cornell.edu>
2025-11-18 15:22:19 -0500

    TTF included

--- ext/BeamTrackingBeamlinesExt/unpack.jl
+++ ext/BeamTrackingBeamlinesExt/unpack.jl
@@ -102,9 +102,7 @@ function universal!(
     if isactive(bendparams)
       error("Tracking through a LineElement containing both RFParams and BendParams not currently defined")
     else
-      gamma_ref = sqrt(1 + beta_gamma_ref^2)
-      beta_ref = beta_gamma_ref / gamma_ref 
-      cav_L = beta_ref*C_LIGHT/(2*rfparams.rf_frequency)
+      cav_L = C_LIGHT/(2*rfparams.rf_frequency)
       drift_L = (L - cav_L)/2
       kc = push(kc, @inline(drift(tm, bunch, drift_L)))
       if !isactive(bmultipoleparams)
